405b9a57e08eea55174d676f8b9e583c9576b627
rey <rheydeguzman891@gmail.com>
2023-05-09 13:26:09 +0800

    Tilemaps
    Modifying stages with Tilemaps

---
+++ CGEN 2d RPG/Assets/VolumeSettings.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume)*20);
+    }
+}
