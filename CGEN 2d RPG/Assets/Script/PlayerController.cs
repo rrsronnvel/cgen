539a77ee366cdc2472f66d31e3b558cb04f6dbb9
rrsronnvel <alinas.ronnvel@gmail.com>
2023-05-25 10:43:09 +0800

    FIX: UI BUTTON / COLLIDER
    FIX: UI BUTTON / COLLIDER  UPDATE

--- CGEN 2d RPG/Assets/Script/PlayerController.cs
+++ CGEN 2d RPG/Assets/Script/PlayerController.cs
@@ -162,7 +162,7 @@ public class PlayerController : MonoBehaviour, IDataPersistence
         // Debug.Log(interactPos);
          Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
 
-        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
+        var collider = Physics2D.OverlapCircle(interactPos, 1f, interactableLayer);
 
         if (collider != null)
         {
