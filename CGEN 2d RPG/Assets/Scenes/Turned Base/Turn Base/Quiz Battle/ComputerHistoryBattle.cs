c4ab80882b56fca666c095167cb08052155d0a9c
rrsronnvel <alinas.ronnvel@gmail.com>
2023-06-03 08:31:02 +0800

    Quiz
    Quiz update

---
+++ CGEN 2d RPG/Assets/Scenes/Turned Base/Turn Base/Quiz Battle/ComputerHistoryBattle.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ComputerHistoryBattle : MonoBehaviour
+{
+    public Text questionText; // Change this to TMP_Text if you're using TextMeshPro
+    public Button[] answerButtons; // An array of Buttons for the answer choices
+    public ComputerHistoryQuestionBank questionBank; // The question bank
+
+    private List<ComputerHistoryQuestion> questions = new List<ComputerHistoryQuestion>();
+    private ComputerHistoryQuestion currentQuestion;
+
+    void Start()
+    {
+        // Display the first question
+        DisplayQuestion(GetRandomQuestion());
+    }
+
+    public ComputerHistoryQuestion GetRandomQuestion()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, questionBank.questions.Count);
+        return questionBank.questions[randomIndex];
+    }
+
+    public void DisplayQuestion(ComputerHistoryQuestion question)
+    {
+        // Set the current question
+        currentQuestion = question;
+
+        // Display the question text
+        questionText.text = question.question;
+
+        // Display the answer choices
+        for (int i = 0; i < question.answers.Length; i++)
+        {
+            // Make sure the button is enabled
+            answerButtons[i].gameObject.SetActive(true);
+
+            // Get the TMP_Text component of the button
+            TMP_Text buttonText = answerButtons[i].GetComponentInChildren<TMP_Text>();
+
+            // Set the button text to the answer choice
+            buttonText.text = question.answers[i];
+
+            // Create a new variable to capture the current value of i
+            int answerIndex = i;
+
+            // Add a click listener to the button
+            answerButtons[i].onClick.AddListener(() => OnAnswerButtonClicked(answerIndex));
+        }
+
+    }
+
+    public void OnAnswerButtonClicked(int buttonIndex)
+    {
+        // Check if the player's answer is correct
+        if (buttonIndex == currentQuestion.correctAnswerIndex)
+        {
+            // The player answered correctly
+            Debug.Log("Correct answer!");
+        }
+        else
+        {
+            // The player answered incorrectly
+            Debug.Log("Incorrect answer.");
+        }
+
+        // Remove the click listeners from the buttons
+        foreach (Button button in answerButtons)
+        {
+            button.onClick.RemoveAllListeners();
+        }
+
+        // Display the next question
+        DisplayQuestion(GetRandomQuestion());
+    }
+}
